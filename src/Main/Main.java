94a549bc867ff5bd4dd88ed1e24687114cd2bf5a
Mykola Kryvoruchko <krivoruchko.kolya@gmail.com>
2024-09-26 13:50:10 +0300

    create package Main and add class Main

---
+++ src/Main/Main.java
@@ -0,0 +1,10 @@
+package Main;
+
+import Menu.MainMenu;
+
+public class Main {
+    public static void main(String[] args) {
+        MainMenu mainMenu = new MainMenu();
+        mainMenu.start();
+    }
+}
